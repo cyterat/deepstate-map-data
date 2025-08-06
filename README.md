5bcfcb8baebec857357935a9a4538495943ed3f5
cyterat <gaelop101@protonmail.com>
2025-08-06 10:26:19 +0300

    Add automated unified GeoJSON dataset with historical data consolidation (#1)
    * [ci] Make the push step branch-agnostic and remove corresponding actions
    
    * [ci] Modify commit message to include date of update
    
    * [update] Reorganize files to improve repo structure
    
    * [update] Rename python script
    
    * [ci] Rename job and change python script path
    
    * [add] Add script to update unified geojson with new data
    
    * [add] Add unified and compressed geojson with all data so far
    
    * [ci] Add job to update unified geojson with new data
    
    * [add] Add screenshot for readme
    
    * [update] Add new section to readme
    
    * [update] Update GeoJSON data - 2025-08-05
    
    * [ci] Remove date from first script commit message
    
    * [fix] Fix ModuleNotFoundError, modify target file import function
    
    * [update] Change exit code to avoid workflow fail
    
    * [remove] Delete updated geojson
    
    * [update] Update GeoJSON data - 2025-08-05
    
    * [fix] Fix the issue of same commit message for 2 workflow jobs
    
    * [remove] Delete updated geojson
    
    * [update] Update GeoJSON data
    
    * [update] Update readme
    
    * [update] Update GeoJSON data - 2025-08-06
    
    ---------
    
    Co-authored-by: GitHub Action <action@github.com>

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 
 # 🔫 DeepState Map Data
 
-The GeoJSON, in `data` folder, contains up-to-date Multipolygons, representing the russian-occupied territory of Ukraine.
+Every GeoJSON, in `data` folder, contains up-to-date Multipolygon, representing the russian-occupied territory of Ukraine.
 
 **Name format:**
 `deepstatemap_data_<update_date>.geojson`
@@ -11,9 +11,82 @@ The GeoJSON, in `data` folder, contains up-to-date Multipolygons, representing t
 **Frequency of updates:**
 Daily, at 03:00 UTC.
 
-## To Do
+    To Do:
+    - Write a simple script using notebook code. ✅
+    - Set up Github Actions for daily updates. ✅
+    - Create a single compressed GeoJSON consolidating all data so far. ✅
+    - Write another script for Github Actions for daily updates of the above mentioned GeoJSON... 🔥
 
-- ~~Write a simple script using notebook code.~~ ✅
-- ~~Set up Github Actions for daily updates.~~ ✅
-- Create a single compressed GeoJSON consolidating all data so far... 🔥
-- Write another script for Github Actions for daily updates of the above mentioned GeoJSON... 🔥
+
+## 📚 `deepstate-map-data.geojson.gz` -- Unified, Compressed Dataset
+
+🟡 __Important:__ All files within the `data` folder will remain untouched, and will continue to be daily updated.
+
+The new compressed file contains all historical geomtries alongside their respective update dates, currently stored in the `data` folder.
+
+**Name format:**
+`deepstate-map-data.geojson.gz`
+
+**Frequency of updates:**
+Daily, at around 03:00 UTC.
+
+Sample Data Structure:
+
+| id  | date       | geometry                                          |   |   |
+|-----|------------|---------------------------------------------------|---|---|
+| 0   | 2024-07-08 | MULTIPOLYGON (((35.20146 45.52334, 35.31126 45... |   |   |
+| 1   | 2024-07-09 | MULTIPOLYGON (((35.20146 45.52334, 35.31126 45... |   |   |
+| ... | ...        | ...                                               |   |   |
+
+\* `date` represents date of update.
+
+### __Accessing compressed data__
+
+If your application or tool does not support gzip-compressed GeoJSON files, below are several different ways I personally used to access/unzip the data.
+
+#### Python (if compressed)
+
+```
+import geopandas as gpd
+from io import StringIO
+import gzip
+
+with gzip.open("deepstate-map-data.geojson.gz", "rt", encoding="utf-8") as f:
+    geojson_str = f.read()
+
+gdf = gpd.read_file(StringIO(geojson_str), crs="EPSG:4326")
+
+print(gdf.head())
+```
+
+#### Python (if uncompressed)
+
+```
+import geopandas as gpd
+
+file = "deepstate-map-data.geojson"
+gdf = gpd.read_file(file, crs="EPSG:4326")
+
+print(gdf.head())
+```
+
+#### Linux Terminal (unzip)
+
+```
+gunzip -c deepstate-map-data.geojson.gz > deepstate-map-data.geojson
+```
+
+#### Windows 7-Zip archiver (unzip)
+
+    1. Right-click `deepstate-map-data.geojson.gz`
+    2. Select "Extract Here"
+    3. The file will decompress to `deepstate-map-data.geojson`
+
+### ⚠️ __Performance Warning__
+
+ Since many features share locations and vary only slightly day to day, rendering all records at once (~400 as of 2025) using tools like __geojson.io__, can cause severe lag.
+
+Try to filter `date` or use sample subsets before using less powerful rendering tools, if you experienced similar performance issues in the past.
+
+_Below: example of multiple layers stacking when loading full dataset without filters_
+<img width="600" src="assets/geojson-rendering-warning.png"/><br>
---
+++ scripts/unify-data.py
@@ -0,0 +1,194 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import logging
+from datetime import datetime
+import gzip
+from io import StringIO
+
+# Prevent "GDAL_DATA is not defined" warning (has to be set before importing geo libraries)
+os.environ['GDAL_DATA'] = os.path.join(f'{os.sep}'.join(sys.executable.split(os.sep)[:-1]), 'Library', 'share', 'gdal')
+
+import geopandas as gpd
+
+
+# Set up logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+# Configuration
+CURRENT_DATE = datetime.now()
+CRS = "EPSG:4326"
+
+SOURCE_FILE = f"deepstatemap_data_{CURRENT_DATE.strftime('%Y%m%d')}.geojson"
+SOURCE_FILE_PATH = os.path.join("data", SOURCE_FILE)
+
+TARGET_FILE = "deepstate-map-data.geojson.gz"
+TARGET_FILE_PATH = os.path.join(TARGET_FILE)
+
+
+def check_source_geojson_existence():
+    """Checks if a new GeoJSON file exists in the specified folder."""
+
+    if os.path.isfile(SOURCE_FILE_PATH):
+        logger.info("Source GeoJSON exists.")
+    else:
+        logger.error(f"Exiting because source GeoJSON with {CURRENT_DATE.strftime('%Y-%m-%d')} update doesn't exist.")
+        sys.exit(1)
+
+
+def check_target_geojson_existence():
+    """Checks if compressed GeoJSON file exists in the repository root."""
+
+    if os.path.isfile(TARGET_FILE_PATH):
+        logger.info("Target GeoJSON exists.")
+    else:
+        logger.error("Exiting because target GeoJSON doesn't exist.")
+        sys.exit(1)
+
+
+def import_source_geojson():
+    """Imports updated GeoJSON geometry into GeoDataFrame"""
+
+    try:
+        # Use 'pyogrio' engine to read geojson (improved performance)
+        new_rows_gdf = gpd.read_file(SOURCE_FILE_PATH, engine="pyogrio")
+
+    except (ValueError, NotImplementedError) as e:
+        logger.warning("Could not use 'pyogrio' engine, switching to 'fiona'...")
+        # Use 'fiona' engine to read geojson (more robust)
+        new_rows_gdf = gpd.read_file(SOURCE_FILE_PATH, engine="fiona")
+
+    except Exception as e:
+        logger.error("Exiting due to error while importing source GeoJSON: {e}")
+        sys.exit(1)
+
+    # Append dictionary with date and geometry to a list
+    new_rows = []
+    if new_rows_gdf.empty:
+        logger.warning("Exiting due to empty GeoDataFrame.")
+        sys.exit(1)
+    else:
+        new_rows.append({
+            "date": CURRENT_DATE.strftime('%Y-%m-%d'),
+            "geometry": new_rows_gdf.geometry.iloc[0]
+        })
+        # Validate if source file contains single feature (row)
+        if new_rows_gdf.shape[0] > 1:
+            logger.error(f"Exiting because expected 1 feature, but source GeoJSON contains {new_rows_gdf.shape[0]}.")
+            sys.exit(1)
+
+    try:
+        # Convert list of dictionaries into GeoDataFrame  
+        source_gdf = gpd.GeoDataFrame(new_rows, geometry="geometry", crs=CRS)
+        logger.info(f"Successfully imported {source_gdf.shape[0]} feature(-s) into GeoDataFrame. Update for {CURRENT_DATE.strftime('%Y-%m-%d')}.")
+        return source_gdf
+
+    except Exception as e:
+        logger.error(f"Exiting due to an error while loading dictionary into GeoDataFrame: {e}")
+        sys.exit(1)
+
+
+def import_target_geojson():
+    """Imports compressed GeoJSON with consolidated records into GeoDataFrame."""
+
+    # Store target (compressed) GeaJSON contents as GeoJSON string
+    with gzip.open(TARGET_FILE_PATH, "rt", encoding="utf-8") as f:
+        geojson_str = f.read()
+        
+    try:
+        # Use 'pyogrio' engine to read geojson (improved performance)
+        target_gdf = gpd.read_file(StringIO(geojson_str), engine="pyogrio")
+
+    except (ValueError, NotImplementedError) as e:
+        logger.warning("Could not use 'pyogrio' engine, switching to 'fiona'...")
+        # Use 'fiona' engine to read geojson (more robust)
+        target_gdf = gpd.read_file(StringIO(geojson_str), engine="fiona")
+
+    except Exception as e:
+        logger.error("Exiting due to error while importing source GeoJSON: {e}")
+        sys.exit(1)
+
+    logger.info(f"Successfully imported target GeoJSON. Last update: {target_gdf.tail(1)["date"]}. Current GeoDataFrame shape: {target_gdf.shape}.")
+
+    # Avoid formatting date as datetime 
+    target_gdf['date'] = target_gdf['date'].dt.strftime('%Y-%m-%d')
+
+    # Drop id column
+    target_gdf = target_gdf.drop(columns="id")
+
+    return target_gdf
+
+
+def unify_datasets(existing_dataset, new_row):
+    """Concatenates target (multiple geometries) and source (single geometry) GeoDataFrames."""
+
+    # Check if 'last' date values match in both GeoDataFrames 
+    if new_row.iloc[-1].loc["date"] in existing_dataset["date"].values:
+        logger.warning(f"Exiting because target dataset already contains the {CURRENT_DATE.strftime('%Y-%m-%d')} update.")
+        sys.exit(0)  
+    else:
+        logger.info("Current update does not yet exist in the target dataset.")
+        try:
+            # Ensure a natural continuation of index for new data
+            new_row.index = [existing_dataset.index.max() + 1]
+            # Concatenate 2 datasets (update appends to the bottom/end of the target)
+            unified_gdf = gpd.pd.concat(
+                [existing_dataset, new_row],
+                ignore_index=False,
+                copy=False
+                )
+            logger.info(f"Successfully concatenated the existing and new data. New GeoDataFrame shape: {unified_gdf.shape}.")
+            return unified_gdf
+        
+        except Exception as e:
+            logger.error(f"Exiting due to error during GeoDataFrames concatenation: {e}")
+            sys.exit(1)
+
+
+def compress_gdf(unified_gdf):
+    """Converts unified GeoDataFrame into JSON stirng, and writes it into a compressed GeoJSON file."""
+
+    # Convert unified GeoDataFrame into a GeoJSON string
+    geojson_str = unified_gdf.to_json()
+
+    try:
+        # Write GeoJSON string into a gzip compressed file 
+        with gzip.open(TARGET_FILE, "wt", encoding="utf-8") as f:
+            f.write(geojson_str)
+        logger.info(f"Successfully exported '{TARGET_FILE}' to the project root.")
+
+    except Exception as e:
+        logger.warning(f"Exiting due to error while writing GeoJSON string into file: {e}")
+        sys.exit(1)
+
+
+def main():
+
+    # Ensure source (updated data) file exists
+    logger.info("Checking if source GeoJSON exists...")
+    check_source_geojson_existence()
+
+    # Ensure target (compressed dataset) file exists
+    logger.info("Checking if target GeoJSON exists...")
+    check_target_geojson_existence()
+    
+    # Import source data
+    logger.info("Importing source GeoJSON...")
+    new_row = import_source_geojson()
+    
+    # Import target data
+    logger.info("Importing target GeoJSON...")
+    existing_dataset = import_target_geojson()
+    
+    # Concatenate dataests into a single GeoDataFrame
+    logger.info("Concatenating source and target datasets...")
+    unified_gdf = unify_datasets(existing_dataset, new_row)
+    
+    # Export unified dataset into a compressed GeoJSON file
+    logger.info("Exporting unified dataset into a compressed file...")
+    compress_gdf(unified_gdf)
+
+if __name__ == "__main__":
+    main()
